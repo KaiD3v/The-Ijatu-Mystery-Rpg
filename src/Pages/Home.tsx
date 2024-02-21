c71b394f38355405b79ede480aa89d5526513b05
Kaique <kaikricardo99@gmail.com>
2024-02-20 22:13:24 -0300

    add second home div + responsivity

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,25 +1,54 @@
+import { Search } from "lucide-react";
+import { UsersRound } from "lucide-react";
+import { Skull } from "lucide-react";
+import MysteriousSvg from "../assets/svg/MysteryouSvg";
+
 export const Home = () => {
   return (
-    <div className="flex flex-col items-center m-10 font-times">
-      <div className="h-10" />
-      <div className="flex flex-col mt-4 border-l p-4"> {/* Adicionando padding de 4 unidades */}
-        <h1 className="text-9xl pr-4 text-white italic">Ijatu Mystery</h1>
-        <p className="m-3 text-2xl ml-1 font-extralight italic text-gray-500">
+    <div className="flex flex-col gap-10 items-center m-10 font-times">
+      <div />
+      <div className="flex flex-col mt-4 border-l p-4">
+        <h1 className="text-6xl sm:text-9xl pr-4 text-white italic">Ijatu Mystery</h1>
+        <p className="m-3 text-lg sm:text-2xl ml-1 font-extralight italic text-gray-500">
           O RPG de Mistério mais pica do Brasil!
         </p>
-        <div className="flex mt-10">
-          <button className="text-github bg-gray-100 border rounded-lg m-1 px-14 py-3 transition duration-300 hover:shadow-lg hover:opacity-100">
+        <div className="flex flex-col sm:flex-row mt-10">
+          <button className="text-github bg-gray-100 border rounded-lg m-1 px-10 py-3 transition duration-300 hover:shadow-lg hover:opacity-100">
             Explorar Conteúdo
           </button>
-          <div className="bg-white w-px" />
-          <button className="text-white border border-white rounded-lg m-1 px-14 py-3 transition duration-500 ease-in-out hover:bg-black hover:border-white hover:opacity-100">
+          <button className="text-white border border-white rounded-lg m-1 px-10 py-3 transition duration-500 ease-in-out hover:bg-black hover:border-white hover:opacity-100">
             Baixar Livros
           </button>
         </div>
       </div>
-      <div className="">
-        
-      </div>
+
+    
+      <div className="w-full max-w-2xl min-w-2xl border-r">
+  <div className="mt-4">
+    <p className="text-2xl font-extralight italic text-gray-500">
+      O que espera por você:
+    </p>
+    <ul className="sm:flex sm:flex-row w-2xl flex-col items-center justify-center gap-4 bg-gray-300 border-none rounded-lg m-1 p-2">
+      <li className="text-black flex items-center">
+        <Search className="mr-2" size={30} color="black" />
+        Enigmas Intrigantes
+      </li>
+      <li className="text-black flex items-center">
+        <UsersRound className="mr-2" size={30} color="black" />
+        Personagens Misteriosos
+      </li>
+      <li className="text-black flex items-center">
+        <Skull className="mr-2" size={30} color="black" />
+        Ambientes Sombrios
+      </li>
+      <li className="text-black flex items-center">
+        <MysteriousSvg/>
+        Conspirações Obscuras
+      </li>
+    </ul>
+  </div>
+</div>
+
     </div>
   );
 };
---
+++ src/assets/svg/MysteryouSvg.tsx
@@ -0,0 +1,30 @@
+const MysteriousSvg = () => {
+    return (
+      <div style={{ width: '30px', height: '30px' }}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          xmlSpace="preserve"
+          version="1.1"
+          viewBox="0 0 847 1058.75"
+          fill="#000000"
+          fillRule="evenodd"
+          clipRule="evenodd"
+        >
+          <path
+            fill="#000000"
+            d="M491 776l84 -90 0 -10 -554 -71 0 34 70 86 400 51zm335 -399l0 -46 -19 75 19 -29zm-212 119l-27 45 0 50 27 -95zm-27 269l26 -43 -26 -21 0 64zm-92 -243l83 11 39 -64 -122 53zm79 265l1 -2 0 -81 -69 74 68 9zm1 -167l0 -75 -93 -12 93 87zm12 66l32 26 168 -273 37 -155c0,-1 1,-2 2,-2l0 -7 -239 397 0 14zm-533 -270l-28 46 61 8 -33 -54zm231 -69c9,-12 23,-17 42,-15 19,2 24,9 15,22 -9,12 -24,17 -43,15 -19,-2 -24,-10 -14,-22zm77 -89l31 3c-6,15 -2,23 12,24 18,2 36,-8 53,-30 26,-35 27,-53 3,-56 -18,-2 -33,5 -46,22 -3,5 -7,11 -12,20l-28 -11c3,-8 6,-13 9,-17 18,-24 51,-33 101,-28 62,7 79,30 50,69 -12,16 -30,28 -54,38 -24,10 -46,13 -68,11 -14,-2 -27,-7 -37,-16l-15 20 -33 -4 34 -45zm110 261l156 -68 158 -257 -46 -74 -388 -50 -116 51 -175 284 42 67 369 47zm-199 -459l-26 43 82 -36 -56 -7zm365 398l-51 177 0 14 239 -397 0 -44 -30 -7 -158 257zm187 -327l-69 -9 37 60 32 -51zm1 65l0 -43 -23 37 23 6zm-251 440l-106 -99c-2,-2 -3,-4 -2,-6l-366 -47c0,1 0,1 -1,2l-79 90 0 17 554 71 0 -28zm-554 -162l0 84 67 -76 -67 -8zm0 184l0 58 53 6 -53 -64z"
+          />
+          <text x="0" y="862" fill="#000000" fontSize="5px" fontWeight="bold" fontFamily="'Helvetica Neue', Helvetica, Arial-Unicode, Arial, Sans-serif">
+            Created by Rafi Al Hakim
+          </text>
+          <text x="0" y="867" fill="#000000" fontSize="5px" fontWeight="bold" fontFamily="'Helvetica Neue', Helvetica, Arial-Unicode, Arial, Sans-serif">
+            from the Noun Project
+          </text>
+        </svg>
+      </div>
+    );
+  };
+  
+  export default MysteriousSvg;
+  
