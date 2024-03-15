225c834ce98a77d9a5cd005668596d19a888e81d
Kaique <kaikricardo99@gmail.com>
2024-03-14 21:08:16 -0300

    new rules

--- src/components/RulesSideBar.tsx
+++ src/components/RulesSideBar.tsx
@@ -14,9 +14,11 @@ export const RulesSideBar: React.FC<OpenMenuProps> = ({ isOpen, toggleMenu }) =>
     >
       <h2 className="text-2xl font-bold mb-4">Regras</h2>
       <ul className="flex flex-col gap-2">
-        <li className="cursor-pointer hover:text-gray-400"><Link to={'/regras/1'}>O Mestre do Jogo</Link></li>
-        <li className="cursor-pointer hover:text-gray-400"><Link to={'/regras/2'}>Como Jogar</Link></li>
-        <li className="cursor-pointer hover:text-gray-400"><Link to={'/regras/3'}>Sistema de Dados</Link></li>
+        <li className="cursor-pointer hover:text-gray-400"><Link to={'/regras/mestre-jogo'}>O Mestre do Jogo</Link></li>
+        <li className="cursor-pointer hover:text-gray-400"><Link to={'/regras/como-jogar'}>Como Jogar</Link></li>
+        <li className="cursor-pointer hover:text-gray-400"><Link to={'/regras/sistema-dados'}>Sistema de Dados</Link></li>
+        <li className="cursor-pointer hover:text-gray-400"><Link to={'/regras/habilidades'}>habilidades</Link></li>
+        <li className="cursor-pointer hover:text-gray-400"><Link to={'/regras/combate'}>Combate</Link></li>
         <li><button onClick={toggleMenu}>Fechar</button></li>
       </ul>  
     </div>
