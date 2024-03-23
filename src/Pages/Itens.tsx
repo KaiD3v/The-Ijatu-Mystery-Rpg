bf22fb3a46cd0d16fb5a852beda4c796293efd92
Kaique <kaikricardo99@gmail.com>
2024-03-23 17:06:58 -0300

    add some itens && add 'imporvisando' section

--- src/Pages/Itens.tsx
+++ src/Pages/Itens.tsx
@@ -1,7 +1,7 @@
 import "../index.css";
 import { useEffect, useState } from "react";
 import weaponsData from "../json/Weapons.json";
-import itemsData from "../json/Items.json"
+import itemsData from "../json/Items.json";
 
 interface Weapon {
   id: string;
@@ -16,8 +16,7 @@ interface Item {
   name: string;
   description: string;
   price: string;
-  type: string
-
+  type: string;
 }
 
 const weapons: Weapon[] = weaponsData.weapons;
@@ -34,9 +33,7 @@ export const Itens = () => {
     );
     setSelectedWeapon(weapon || null);
 
-    const item = items.find(
-      (item) => item.id === (selectedItem?.id || "")
-    );
+    const item = items.find((item) => item.id === (selectedItem?.id || ""));
     setSelectedItem(item || null);
   }, [selectedWeapon, selectedItem]);
 
@@ -100,6 +97,48 @@ export const Itens = () => {
             ))}
           </tbody>
         </table>
+      </div>
+      <div className="flex flex-col my-5 justify-center items-center gap-4 text-initial content-center">
+        <h1 className="m-2 text-2xl">Improvisando</h1>
+        <p>
+          Às vezes, os aventureiros se deparam com situações onde não têm acesso
+          aos equipamentos adequados. Nessas circunstâncias, a criatividade e a
+          habilidade em improvisar podem ser suas melhores aliadas. Abaixo estão
+          algumas ideias e sugestões para improvisar itens e soluções em
+          momentos de necessidade:
+        </p>
+        <ul>
+          <li>
+            <strong>Armas Improvisadas:</strong> Utilize objetos do ambiente,
+            como pedras, paus ou ferramentas, para criar armas improvisadas em
+            situações de combate desesperadoras.
+          </li>
+          <li>
+            <strong>Utensílios de Sobrevivência:</strong> Aproveite recursos
+            naturais, como folhas, galhos e cascas de árvores, para construir
+            abrigos temporários, armadilhas para caça ou ferramentas de pesca.
+          </li>
+          <li>
+            <strong>Disfarces e Camuflagem:</strong> Use roupas e tecidos
+            disponíveis para se misturar ao ambiente e passar despercebido por
+            inimigos ou animais selvagens.
+          </li>
+          <li>
+            <strong>Truques e Armadilhas:</strong> Improvise dispositivos
+            simples, como armadilhas de corda ou armadilhas improvisadas, para
+            desativar armadilhas inimigas ou atrasar perseguidores.
+          </li>
+          <li>
+            <strong>Comunicação e Sinais:</strong> Use sinais de fumaça, pedras
+            ou ramos para comunicar com aliados à distância ou enviar mensagens
+            codificadas para reforços.
+          </li>
+        </ul>
+        <p>
+          Lembrando sempre que a eficácia dessas improvisações depende da
+          criatividade, do conhecimento do ambiente e das habilidades dos
+          personagens envolvidos.
+        </p>
       </div>
     </div>
   );
